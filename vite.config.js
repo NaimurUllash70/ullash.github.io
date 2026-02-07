0eca6a6d9c3e4aa12e8305f50f124f571171f8ea
Ullash <nr.naimurullash8770@gmail.com>
2026-02-08 02:14:12 +0600

    fix: set vite base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",   // 🔴 MUST (ullash.github.io = user site)
+  base: "/ullash.github.io/",   // 🔥 VERY IMPORTANT
 });
